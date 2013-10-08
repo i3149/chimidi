8ae13641c7aee77a10e6e88b0d3f475ab68f33ee
Ian Pye <ianpye@gmail.com>
2013-10-08 15:17:27 -0700

    Hooked up keyboard plus rationalized note layout

--- public/javascripts/chimidi.js
+++ public/javascripts/chimidi.js
@@ -2,6 +2,33 @@ define(['promenade', 'jquery', 'midi', 'monkey'],
        function(Promenade, $, MIDI, Monkey) {
   'use strict';
 
+         var key2Note = {
+           65: "C2",
+           83: "D2",
+           68: "E2",
+           70: "F2",
+           71: "G2",
+           72: "A2",
+           74: "B2",
+           75: "C3",
+           76: "D3",
+           186: "E3",
+           222: "F3",
+           219: "G3",
+           221: "A3",
+           220: "B3",           
+           81: "Db2",
+           87: "Eb2",
+           69: "Gb2",
+           82: "Ab2",
+           84: "Bb2",
+           89: "Db3",
+           85: "Eb3",
+           73: "Gb3",
+           79: "Ab3",
+           80: "Bb3",
+         }
+
   MIDI.loadPlugin({
     soundfontUrl: './soundfonts/',
     instrument: 'acoustic_grand_piano',
@@ -20,6 +47,24 @@ define(['promenade', 'jquery', 'midi', 'monkey'],
         });
 
         return false;
+      });
+
+      $(document).keyup(function(e) {
+        var note = MIDI.keyToNote[key2Note[e.which]];
+        var volume = 127;
+
+        if (note) {          
+          chmidi.doNoteOff(note, volume)
+        }
+      });
+
+      $(document).keydown(function(e) {
+        var note = MIDI.keyToNote[key2Note[e.which]];
+        var volume = 127;
+
+        if (note) {
+          chmidi.doNoteOn(note, volume)
+        }
       });
     }
   });
